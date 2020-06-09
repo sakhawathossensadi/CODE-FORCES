e4f6a0cd7059f9d14fd88ff08a8bec7da9f83a6f
MD. SAKHAWAT HOSSEN <newtonss919@gmail.com>
2020-06-09 16:47:39 +0600

    Add files via upload

---
+++ A. Bear and Big BrotherCODEFORCES.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,count=0;
+    scanf("%d %d",&a,&b);
+    while(a<=b)
+    {
+        a=a*3;
+        b=b*2;
+        count++;
+    }
+    printf("%d\n",count);
+    return 0;
+}
