69380ac4ebcbe374244ecf25abfba80146e76fad
MD. SAKHAWAT HOSSEN <newtonss919@gmail.com>
2020-07-13 15:07:53 +0600

    Add files via upload

---
+++ A. Omkar and CompletionCODEFORCES.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,t,n,k;
+    scanf("%d",&t);
+    for(k=0;k<t;k++)
+    {
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("1 ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
